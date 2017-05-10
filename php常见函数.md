acfaefa1986083c1731f72b2788ad1fdcc4122b9
wushijie <451164037@qq.com>
2017-05-10 10:51:46 +0800

    mysql扩展

--- php常见函数.md
+++ php常见函数.md
@@ -2,6 +2,16 @@
 
 ###is_a()
     如果对象属于该类或该类是此对象的父类则返回 TRUE
+    instanceof() 比较
 ###in_array()
-    第三个参数 true  类型的判断  默认为false(demo 看手册)
+    bool in_array ( mixed $needle , array $haystack [, bool $strict = FALSE ] )
+    $needle 区分大小写
+    严格模式 比较类型 同array_search()
+    非严格模式:
+`$arr = ['a','b'];var_dump(in_array(0,$arr));var_dump(in_array(true,$arr));`
 
+###htmlspecialchars() && htmlspecialchars_decode()
+
+###url_encode() && rawurlencode()
+    同：对除·-_.·之外所有非字母数字字符替换为%后跟两位十六制数
+    异：url_encode 将空格转化为·+·
---
+++ test.php
@@ -0,0 +1,223 @@
+<?php
+
+//class WS
+//{
+//    var $master;
+//    var $sockets = array();
+//    var $debug = false;
+//    var $handshake = false;
+//
+//    function __construct($address, $port)
+//    {
+//        $this->master = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("socket_create() failed");
+//        socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1) or die("socket_option() failed");
+//        socket_bind($this->master, $address, $port) or die("socket_bind() failed");
+//        socket_listen($this->master, 20) or die("socket_listen() failed");
+//
+//        $this->sockets[] = $this->master;
+//        $this->say("Server Started : " . date('Y-m-d H:i:s'));
+//        $this->say("Listening on   : " . $address . " port " . $port);
+//        $this->say("Master socket  : " . $this->master . "\n");
+//
+//        while (true) {
+//            $socketArr = $this->sockets;
+//            $write     = NULL;
+//            $except    = NULL;
+//            socket_select($socketArr, $write, $except, NULL);  //自动选择来消息的socket 如果是握手 自动选择主机
+//            foreach ($socketArr as $socket) {
+//                if ($socket == $this->master) {  //主机
+//                    $client = socket_accept($this->master);
+//                    if ($client < 0) {
+//                        $this->log("socket_accept() failed");
+//                        continue;
+//                    } else {
+//                        $this->connect($client);
+//                    }
+//                } else {
+//                    $this->log("^^^^");
+//                    $bytes = @socket_recv($socket, $buffer, 2048, 0);
+//                    $this->log("^^^^");
+//                    if ($bytes == 0) {
+//                        $this->disConnect($socket);
+//                    } else {
+//                        if (!$this->handshake) {
+//                            $this->doHandShake($socket, $buffer);
+//                        } else {
+//                            $buffer = $this->decode($buffer);
+//                            $this->send($socket, $buffer);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    function send($client, $msg)
+//    {
+//        $this->log("> " . $msg);
+//        $msg = $this->frame($msg);
+//        socket_write($client, $msg, strlen($msg));
+//        $this->log("! " . strlen($msg));
+//    }
+//
+//    function connect($socket)
+//    {
+//        array_push($this->sockets, $socket);
+//        $this->say("\n" . $socket . " CONNECTED!");
+//        $this->say(date("Y-n-d H:i:s"));
+//    }
+//
+//    function disConnect($socket)
+//    {
+//        $index = array_search($socket, $this->sockets);
+//        socket_close($socket);
+//        $this->say($socket . " DISCONNECTED!");
+//        if ($index >= 0) {
+//            array_splice($this->sockets, $index, 1);
+//        }
+//    }
+//
+//    function doHandShake($socket, $buffer)
+//    {
+//        $this->log("\nRequesting handshake...");
+//        $this->log($buffer);
+//        list($resource, $host, $origin, $key) = $this->getHeaders($buffer);
+//        $this->log("Handshaking...");
+//        $upgrade = "HTTP/1.1 101 Switching Protocol\r\n" .
+//            "Upgrade: websocket\r\n" .
+//            "Connection: Upgrade\r\n" .
+//            "Sec-WebSocket-Accept: " . $this->calcKey($key) . "\r\n\r\n";  //必须以两个回车结尾
+//        $this->log($upgrade);
+//        $sent            = socket_write($socket, $upgrade, strlen($upgrade));
+//        $this->handshake = true;
+//        $this->log("Done handshaking...");
+//
+//        return true;
+//    }
+//
+//    function getHeaders($req)
+//    {
+//        $r = $h = $o = $key = null;
+//        if (preg_match("/GET (.*) HTTP/", $req, $match)) {
+//            $r = $match[1];
+//        }
+//        if (preg_match("/Host: (.*)\r\n/", $req, $match)) {
+//            $h = $match[1];
+//        }
+//        if (preg_match("/Origin: (.*)\r\n/", $req, $match)) {
+//            $o = $match[1];
+//        }
+//        if (preg_match("/Sec-WebSocket-Key: (.*)\r\n/", $req, $match)) {
+//            $key = $match[1];
+//        }
+//
+//        return array($r, $h, $o, $key);
+//    }
+//
+//    function calcKey($key)
+//    {
+//        //基于websocket version 13
+//        $accept = base64_encode(sha1($key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true));
+//
+//        return $accept;
+//    }
+//
+//    function decode($buffer)
+//    {
+//        $len = $masks = $data = $decoded = null;
+//        $len = ord($buffer[1]) & 127;
+//
+//        if ($len === 126) {
+//            $masks = substr($buffer, 4, 4);
+//            $data  = substr($buffer, 8);
+//        } else if ($len === 127) {
+//            $masks = substr($buffer, 10, 4);
+//            $data  = substr($buffer, 14);
+//        } else {
+//            $masks = substr($buffer, 2, 4);
+//            $data  = substr($buffer, 6);
+//        }
+//        for ($index = 0; $index < strlen($data); $index++) {
+//            $decoded .= $data[$index] ^ $masks[$index % 4];
+//        }
+//
+//        return $decoded;
+//    }
+//
+//    function frame($s)
+//    {
+//        $a = str_split($s, 125);
+//        if (count($a) == 1) {
+//            return "\x81" . chr(strlen($a[0])) . $a[0];
+//        }
+//        $ns = "";
+//        foreach ($a as $o) {
+//            $ns .= "\x81" . chr(strlen($o)) . $o;
+//        }
+//
+//        return $ns;
+//    }
+//
+//
+//    function say($msg = "")
+//    {
+//        echo $msg . "\n";
+//    }
+//
+//    function log($msg = "")
+//    {
+//        if ($this->debug) {
+//            echo $msg . "\n";
+//        }
+//    }
+//}
+//
+//
+//new WS('127.0.0.1', 443);
+
+//$arr = [
+//    ['id' => 1, 'no' => '110'],
+//    ['id' => 2, 'no' => '111'],
+//    ['id' => 3, 'no' => '112'],
+//];
+//
+//$map = array_map(function ($v) {
+//    if ($v['no'] == 110) {
+//        return $v['no'];
+//    }
+//}, $arr);
+//var_dump($map);
+
+
+
+//二进制转化为十进制 无线循环
+//$needPay = (8.3121*300);
+//echo round($needPay,10);
+//echo PHP_EOL;
+//echo (int)(round($needPay,7)*1000);
+
+//echo ($needPay);
+//echo PHP_EOL;
+//echo (int)$needPay*1000;
+//echo PHP_EOL;
+//echo round($needPay,4);
+
+
+//echo (int)(6.9631*100000*3).PHP_EOL;
+//
+//echo (int)(6.9631*3*100000).PHP_EOL;
+//
+//echo (int)(6.9631*300*1000).PHP_EOL;
+//
+//echo (int)(6.9631*300*1000).PHP_EOL;
+//
+//echo floor((0.1+0.7)*10000);
+//
+//PDO::MYSQL_ATTR_INIT_COMMAND;
+
+try{
+    $dir = dir('D:/kcevn/wwww');
+    var_dump($dir);
+}catch (Exception $e){
+    echo $e->getMessage();
+}
